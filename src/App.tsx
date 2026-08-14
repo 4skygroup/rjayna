7e2d3ac77b03f92a11563812a18e62c42e6e3a42
Antoine-Chen <antoinecoc2408@gmail.com>
2026-08-14 14:22:53 +0200

    achitecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import ScrollToTop from "./components/ScrollToTop.tsx";
+import {Route, Routes} from "react-router-dom";
+import Home from "./pages/Home.tsx";
+
 function App() {
 
   return (
       <>
+          <ScrollToTop />
+          <Routes>
+              <Route path="/" element={<Home />} />
+
+          </Routes>
           <h1 className="text-2xl">CONFIGURATIONN</h1>
           <p className="font-raleway tracking-raleway">Titre en Raleway</p>
           <p className="font-cormorant tracking-cormorant">Texte en Cormorant</p>
