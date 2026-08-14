48daa5187dc68770aa420496c3b7a379231b8709
Antoine-Chen <antoinecoc2408@gmail.com>
2026-08-14 14:12:49 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+      <>
+          <h1 className="text-2xl">CONFIGURATIONN</h1>
+          <p className="font-raleway tracking-raleway">Titre en Raleway</p>
+          <p className="font-cormorant tracking-cormorant">Texte en Cormorant</p>
+          <span className="font-didot tracking-didot">Texte en GFS Didot</span>
+      </>
+  )
+}
+
+export default App
