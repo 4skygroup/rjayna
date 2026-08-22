e307f299b7fcfbe8f59d78431160e5f53115e914
Antoine-Chen <antoinecoc2408@gmail.com>
2026-08-22 12:37:53 +0200

    fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -38,7 +38,7 @@ export default function Navbar() {
                     className="hidden rounded-full bg-black px-6 py-2.5 font-raleway tracking-raleway text-sm uppercase text-cream transition-opacity hover:opacity-90 md:inline-block"
                 >
                     <p className="font-raleway tracking-raleway text-xs uppercase text-white [transform:scaleY(0.85)] ">
-                        Contacter - nous
+                        Contactez - nous
                     </p>
                 </Link>
 
@@ -89,7 +89,7 @@ export default function Navbar() {
                         onClick={() => setIsOpen(false)}
                         className="rounded-full bg-cream px-8 py-3 font-raleway tracking-raleway text-sm uppercase text-black"
                     >
-                        Contacter
+                        Contactez - nous
                     </Link>
                 </div>
             </div>
